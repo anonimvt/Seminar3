77b9752b08d048b5052a7d74743da9744d1a5a7b
anonimvt <75793665+anonimvt@users.noreply.github.com>
2023-02-14 10:20:55 +0400

    Task 19

---
+++ Task19/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+string? number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Это число является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Это число не является палиндромом");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} -Это число не является пятизначным");
+}
